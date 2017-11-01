86850d5ebfd9d961c1bf99ab3cdd8ffdd0fa1683
Deivid Diaz <deiby2494@gmail.com>
2017-10-31 21:22:46 -0600

    Correcciones en el server de Node.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-	var port_number = server.listen(process.env.PORT || 8080);
-	app.listen(port_number);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
 });
+
+let port = process.env.PORT || 8080;
+app.listen(port);
