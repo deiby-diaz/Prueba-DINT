7ef01a1a80954e09e8060a5b0900fe4f54aa5e2c
Deivid Diaz <deiby2494@gmail.com>
2017-10-31 21:05:16 -0600

    Correcciones del server

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
+	var port_number = server.listen(process.env.PORT || 8080);
+	app.listen(port_number);
 });
