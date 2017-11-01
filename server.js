6c020860ce5f0837ec27f4573d99ddbfa875d687
Deivid Diaz <deiby2494@gmail.com>
2017-10-31 21:56:47 -0600

    Correcciones del server en Node.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
+//Look for statics first
+app.use(express.static(path.join(__dirname, '')));
+//Return the index for any other GET request
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname, '')});
 });
-
-let port = process.env.PORT || 8080;
-app.listen(port);
+app.listen(8080, function(){
+    console.log((new Date()) + " Server is listening on port 8080");
+});   
